1bdbc5d1b382348fbd595f455194afdbe96bd6a5
Tony C <tonychiesa92@gmail.com>
2019-06-19 16:41:17 -0400

    added pseudo-code to map variables and logic for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//select random # b/w 19-120, then display
+//set random values to crystals b/w 1-12, hide value until clicked on
+// start total score=0
+// on click add value of crystal to total score
+// if (total score= random#)
+//      win++
+//      "winner! play again!"
+// reset all random #'s and score 
+//else if( total score > random #)
+//      losses ++
+//      "you lost, play agian"
+//reset all random #'s and score 
