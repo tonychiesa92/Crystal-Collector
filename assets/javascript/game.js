0dd61a58e28b39c3dccbd98a216ec00de5ef9db7
Tony C <tonychiesa92@gmail.com>
2019-06-21 19:34:49 -0400

    reset function working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,10 +11,6 @@ var lossses = 0;
 
 
 $("#target-num").text(randomTarget);
-$("#c1").text(randomC1);
-$("#c2").text(randomC2);
-$("#c3").text(randomC3);
-$("#c4").text(randomC4);
 $("#total-score").text(totalScore);
 $("#numberWins").text("Wins: " + wins);
 $("#numberLosses").text("Losses: " + lossses);
@@ -68,56 +64,27 @@ $("#c4").on("click", function () {
     }
 });
 
-function winFunction(){
-    wins ++;
+function winFunction() {
+    wins++;
     $("#numberWins").text("Wins: " + wins);
+    reset();
 }
 
-function looseFunction(){
-    lossses ++;
-    $("#numberWins").text("Losses: " + lossses);
+function looseFunction() {
+    lossses++;
+    $("#numberLosses").text("Losses: " + lossses);
+    reset();
 }
 
+function reset() {
+    randomTarget = Math.floor(Math.random() * 101 + 19);
+    $("#target-num").text(randomTarget);
 
-
-//select random # b/w 19-120, then display
-//set random values to crystals b/w 1-12, hide value until clicked on
-// start total score=0
-// on click add value of crystal to total score
-// if (total score= random#)
-//      win++
-//      "winner! play again!"
-// reset all random #'s and score 
-//else if( total score > random #)
-//      losses ++
-//      "you lost, play agian"
-//reset all random #'s and score 
-
-// $(".crystal").on("click", function(){
-
-//     // determine which button was pressed--alert
-//     // get value of that id
-//     // add to total score
-
-
-//     // run function: winner/losser with a reset
-//         //status(totalScore)
-//     // var crystal = $(".crystal");
-//     // //     $(this).val({
-//     // //         "data-random": random
-//     //         totalScore += $(this).val();
-
-//     //     });
-//     // });
-// });
-
-// function status(value){
-//     //did they win
-//         // reset
-
-//     //did they lose
-//         // reset
-
-//     //if no win or loss 
-//         // display new total score
-// }
+    randomC1 = Math.floor(Math.random() * 12) + 1;
+    randomC2 = Math.floor(Math.random() * 12) + 1;
+    randomC3 = Math.floor(Math.random() * 12) + 1;
+    randomC4 = Math.floor(Math.random() * 12) + 1;
+    
+    totalScore = 0;
+    $("#total-score").text(totalScore);
+}
